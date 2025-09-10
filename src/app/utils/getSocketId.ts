d04c011bdd11e3bf764557d8c27fab479329cf36
Shazzadul-Shakib <shakib1186@gmail.com>
2025-09-10 08:51:37 +0600

    removed unused codes

--- src/app/utils/getSocketId.ts
+++ src/app/utils/getSocketId.ts
@@ -1,11 +1,3 @@
-// // Store user socket connections
-// export const userSockets = new Map<string, string>(); // userId -> socketId
-
-// // Helper function to get user's socket ID
-// export const getUserSocketId = (userId: string): string | undefined => {
-//   return userSockets.get(userId);
-// };
-
 // Store user socket connections
 export const userSockets = new Map<string, string>(); // userId -> socketId
 
