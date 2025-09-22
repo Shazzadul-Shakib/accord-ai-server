aeeaffe30014b25ee768b746e587444b42282e2e
Shazzadul-Shakib <shakib1186@gmail.com>
2025-09-22 06:21:31 +0600

    fix(cors): allow localhost origin and update sameSite cookie policy

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -27,14 +27,14 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
   res.cookie('refreshToken', result.refreshToken, {
     httpOnly: config.node_env === 'production',
     secure: true,
-    sameSite: config.node_env === 'production' ? 'none' : 'lax',
+    sameSite: config.node_env === 'production' ? 'strict' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   res.cookie('accessToken', result.accessToken, {
     httpOnly: config.node_env === 'production',
     secure: true,
-    sameSite: config.node_env === 'production' ? 'none' : 'lax',
+    sameSite: config.node_env === 'production' ? 'strict' : 'lax',
     maxAge: 15 * 60 * 1000, 
   });
   
