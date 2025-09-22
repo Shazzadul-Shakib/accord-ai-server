ace8cff06834c4ca389ee4801e2510dbe2f86a8d
Shazzadul-Shakib <shakib1186@gmail.com>
2025-09-22 10:18:03 +0600

    fix(cors): allow localhost:3000 as additional origin for development

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ async function main() {
     // Initialize Socket.IO
     io = new SocketServer(server, {
       cors: {
-        origin: ['https://accord-ai-client.vercel.app'],
+        origin: ['https://accord-ai-client.vercel.app',"http://localhost:3000"],
         methods: ['GET', 'POST'],
         credentials: true,
       },
